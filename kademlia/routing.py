7f122724cb1c504b279c0fc695183173e137daac
eloahman <ainsof@live.cn>
2016-08-04 16:38:03 +0800

    bug fix

--- kademlia/routing.py
+++ kademlia/routing.py
@@ -1,6 +1,7 @@
 import heapq
 import time
 import operator
+import asyncio
 from collections import OrderedDict
 
 from kademlia.utils import OrderedSet, sharedPrefix
@@ -158,7 +159,7 @@ class RoutingTable(object):
             self.splitBucket(index)
             self.addContact(node)
         else:
-            self.protocol.callPing(bucket.head())
+            asyncio.ensure_future(self.protocol.callPing(bucket.head()))
 
     def getBucketFor(self, node):
         """
