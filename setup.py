44007c521167964b6498d8c4ee0033ef1f5cfa51
Brian Muller <bamuller@gmail.com>
2019-01-16 11:21:19 -0500

    added long description to setup

--- setup.py
+++ setup.py
@@ -6,10 +6,23 @@ setup(
     name="kademlia",
     version=kademlia.__version__,
     description="Kademlia is a distributed hash table for decentralized peer-to-peer computer networks.",
+    long_description=open("README.md").read(),
     author="Brian Muller",
     author_email="bamuller@gmail.com",
     license="MIT",
     url="http://github.com/bmuller/kademlia",
     packages=find_packages(),
-    install_requires=["rpcudp>=3.0.0"]
+    install_requires=open("requirements.txt").readlines(),
+    classifiers=[
+      "Development Status :: 5 - Production/Stable",
+      "Intended Audience :: Developers",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+      "Programming Language :: Python",
+      "Programming Language :: Python :: 3",
+      "Programming Language :: Python :: 3.5",
+      "Programming Language :: Python :: 3.6",
+      "Programming Language :: Python :: 3.7",
+      "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
 )
