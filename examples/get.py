d8812c75698f9641a8fac09f306b93335bbb90c5
arteger <artegerartegerasd@gmail.com>
2020-12-03 18:54:46 +0200

    Changed examples to use predefined ports

--- examples/get.py
+++ examples/get.py
@@ -4,9 +4,9 @@ import sys
 
 from kademlia.network import Server
 
-if len(sys.argv) != 4:
-    print("Usage: python get.py <bootstrap node> <bootstrap port> <key>")
-    sys.exit(1)
+# if len(sys.argv) != 4:
+#     print("Usage: python get.py <bootstrap node> <bootstrap port> <key>")
+#     sys.exit(1)
 
 handler = logging.StreamHandler()
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -18,11 +18,11 @@ log.setLevel(logging.DEBUG)
 async def run():
     server = Server()
     await server.listen(8469)
-    bootstrap_node = (sys.argv[1], int(sys.argv[2]))
+    bootstrap_node = ("127.0.0.1", 8468)
     await server.bootstrap([bootstrap_node])
 
-    result = await server.get(sys.argv[3])
+    result = await server.get("test_key")
     print("Get result:", result)
-    server.stop()
+    # server.stop()
 
 asyncio.run(run())
--- examples/set.py
+++ examples/set.py
@@ -4,9 +4,9 @@ import sys
 
 from kademlia.network import Server
 
-if len(sys.argv) != 5:
-    print("Usage: python set.py <bootstrap node> <bootstrap port> <key> <value>")
-    sys.exit(1)
+# if len(sys.argv) != 5:
+#     print("Usage: python set.py <bootstrap node> <bootstrap port> <key> <value>")
+#     sys.exit(1)
 
 handler = logging.StreamHandler()
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -17,10 +17,12 @@ log.setLevel(logging.DEBUG)
 
 async def run():
     server = Server()
-    await server.listen(8469)
-    bootstrap_node = (sys.argv[1], int(sys.argv[2]))
+    await server.listen(8470)
+    bootstrap_node = ("127.0.0.1", 8468)
     await server.bootstrap([bootstrap_node])
-    await server.set(sys.argv[3], sys.argv[4])
-    server.stop()
+    await server.set("test_key", "Hello")
+    while True:
+        pass
+    # server.stop()
 
 asyncio.run(run())
